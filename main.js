2ce2d46517de7a250d0ed7ab0c00b837337d579f
Dmitriy <aua008@gmail.com>
2024-10-23 15:08:52 +0300

    Added new functions and added invite collaborate card

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
 	const playOrPauseBtn = document.getElementById('play-or-pause')
 	const muteBtn = document.getElementById('mute')
 	const fullScreenBtn = document.getElementById('fullscreen')
+	const volumeSlider = document.getElementById('volume-slider')
+	const currentTime = document.getElementById('controls-current-time')
+	const duration = document.getElementById('controls-duration')
+	let volumeSliderValue = 0.5
 
 	function toggleSiblingElement(parentElement, element, showFirst = false) {
 		if (showFirst) {
@@ -19,6 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
+	function chooseVolume() {
+		player.volume = volumeSlider.value
+		volumeSliderValue = volumeSlider.value
+	}
+
 	function toggleFullScreen() {
 		if (player.requestFullscreen) {
 			player.requestFullscreen()
@@ -32,12 +41,32 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	function playOrPause() {
+		const playCircle = document.querySelector('.play-circle')
+		let timerId = null
+
 		if (player.paused) {
 			player.play()
 			toggleSiblingElement(playOrPauseBtn, 'svg')
+			toggleSiblingElement(playCircle, 'svg', true)
+			playCircle.style.transform = 'translate(-50%, -50%) scale(1.7)'
+			playCircle.style.opacity = '1'
+			timerId = setTimeout(() => {
+				playCircle.style.opacity = '0'
+				playCircle.style.transform = 'translate(-50%, -50%) scale(1.2)'
+				clearTimeout(timerId)
+			}, 300)
 		} else {
 			player.pause()
 			toggleSiblingElement(playOrPauseBtn, 'svg', true)
+			toggleSiblingElement(playCircle, 'svg')
+			playCircle.style.transform = 'translate(-50%, -50%) scale(1.7)'
+			playCircle.style.opacity = '1'
+
+			timerId = setTimeout(() => {
+				playCircle.style.opacity = '0'
+				playCircle.style.transform = 'translate(-50%, -50%) scale(1.2)'
+				clearTimeout(timerId)
+			}, 300)
 		}
 	}
 
@@ -45,14 +74,33 @@ document.addEventListener('DOMContentLoaded', () => {
 		if (!player.muted) {
 			player.muted = true
 			toggleSiblingElement(muteBtn, 'svg')
+			volumeSlider.value = 0
 		} else {
 			player.muted = false
 			toggleSiblingElement(muteBtn, 'svg', true)
+			volumeSlider.value = volumeSliderValue
 		}
+	}
+
+	function formatTime(seconds) {
+		const minutes = Math.floor(seconds / 60)
+		const sec = Math.floor(seconds % 60)
+		console.log(minutes)
+
+		return (
+			(minutes < 10 ? '0' : '') + minutes + ':' + (sec < 10 ? '0' : '') + sec
+		)
 	}
 
 	playOrPauseBtn.addEventListener('click', playOrPause)
 	muteBtn.addEventListener('click', mute)
 	fullScreenBtn.addEventListener('click', toggleFullScreen)
+	volumeSlider.addEventListener('input', chooseVolume)
 	player.addEventListener('click', playOrPause)
+	player.addEventListener('loadedmetadata', () => {
+		duration.innerText = formatTime(player.duration)
+	})
+	player.addEventListener('timeupdate', () => {
+		currentTime.innerText = formatTime(player.currentTime)
+	})
 })
